400d63530cac714906ccb42523a0b801820a3e0b
Davina <davinahouquet@hotmail.com>
2023-04-04 11:11:21 +0200

    Méthode listerContratsEquipe testée

--- Contrat.php
+++ Contrat.php
@@ -3,7 +3,7 @@
 class Contrat{
     private Joueur $_joueur;
     private Equipe $_equipe;
-    private DateTime $_dateDebutSaison;
+    private string $_dateDebutSaison;
 
     public function __construct(Joueur $joueur, Equipe $equipe, string $dateDebutSaison)
     {
@@ -12,6 +12,46 @@ class Contrat{
         $this->_dateDebutSaison = $dateDebutSaison;
         $this->_joueur->setContrats($this);
         $this->_equipe->setContrats($this);
+    }
+
+    public function getJoueur()
+    {
+        return $this->_joueur;
+    }
+ 
+    public function setJoueur(Joueur $joueur)
+    {
+        $this->_joueur = $joueur;
+
+        return $this;
+    }
+    
+    public function getEquipe()
+    {
+        return $this->_equipe;
+    }
+
+    public function setEquipe(Equipe $equipe)
+    {
+        $this->_equipe = $equipe;
+
+        return $this;
+    }
+    
+    public function getDateDebutSaison()
+    {
+        return $this->_dateDebutSaison;
+    }
+
+    public function setDateDebutSaison(string $dateDebutSaison)
+    {
+        $this->_dateDebutSaison = $dateDebutSaison;
+
+        return $this;
+    }
+
+    public function __toString(){
+        return $this->_joueur . ", " . $this->_equipe . ", " . $this->_dateDebutSaison;
     }
 }
 
